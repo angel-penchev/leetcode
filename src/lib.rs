0b5051339b6f2adc37074b674a5babb5acc88832
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-10-05 19:18:38 +0300

    chore: add leading zero to file names
    As LeetCode solutions are more than 1000, added a leading 0 to the
    module names.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-pub mod lc001_two_sum;
-pub mod lc002a_add_two_numbers;
-pub mod lc002b_add_two_numbers;
-pub mod lc003a_longest_substring_without_repeating_characters;
-pub mod lc003b_longest_substring_without_repeating_characters;
-pub mod lc004_median_of_two_sorted_arrays;
-pub mod lc005_longest_palindromic_substring;
+pub mod lc0001_two_sum;
+pub mod lc0002a_add_two_numbers;
+pub mod lc0002b_add_two_numbers;
+pub mod lc0003a_longest_substring_without_repeating_characters;
+pub mod lc0003b_longest_substring_without_repeating_characters;
+pub mod lc0004_median_of_two_sorted_arrays;
+pub mod lc0005_longest_palindromic_substring;
